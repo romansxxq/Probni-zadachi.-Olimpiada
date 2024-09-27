c530009878d77bdc013186939a0ba764b931d24f
Roman <asssfsa010@gmail.com>
2024-09-27 15:07:03 +0300

    Add project files.

---
+++ Пробні задачі. Олімпіада/Task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пробні_задачі.Олімпіада
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int a = int.Parse(numbers[0]);
+            int b = int.Parse(numbers[1]);
+
+            int sum = a + b;
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Пробні задачі. Олімпіада/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пробні_задачі.Олімпіада
+{
+    class Task2
+    {
+        static int NZP(int[] args)
+        {
+            int n = args.Length;
+            int[] nzp = new int[n];
+
+            for (int i = 0; i < n; i++) 
+                nzp[i] = 1;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < i; j++)
+                    if (args[i] > args[j] && nzp[i] < nzp[j] + 1)
+                        nzp[i] = nzp[j] + 1;
+
+            return nzp.Max();
+
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] args = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int res = NZP(args);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
